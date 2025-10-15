7e0f2aa812998dab6ce6c2f669a99aa932a36854
FZoltan40 <farkasz@kkszki.hu>
2025-10-15 07:56:43 +0200

    Login gui is ready.

---
+++ TraderWpf/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TraderWpf
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void logButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("✔Login");
+        }
+    }
+}
--- TraderWpf/MainWindow.xaml.cs
+++ TraderWpf/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TraderWpf
 {
@@ -23,7 +10,7 @@ namespace TraderWpf
         public MainWindow()
         {
             InitializeComponent();
-            StartWindow.Navigate(new RegisterPage());
+            StartWindow.Navigate(new LoginPage());
         }
     }
 }
